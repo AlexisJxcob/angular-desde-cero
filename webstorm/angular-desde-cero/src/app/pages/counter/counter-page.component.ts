f3dd363d2219d988b41deb4948d7a6d3f9cae83f
Alexis Jacob <ferjacobka@gmail.com>
2026-08-23 00:04:03 -0400

    chore: component

---
+++ webstorm/angular-desde-cero/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  template: `
+    <h1>hOLA MUNDO DESDE ANGULAR</h1>
+    <h2>Counter Page</h2>`,
+})
+export class CounterPageComponent {
+
+}
